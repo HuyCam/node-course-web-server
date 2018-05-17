b9af4e415ec30a6ffec8f8300cbc222f53f0adaa
HuyCam <camhuy165@gmail.com>
2018-05-16 19:37:16 -0500

    Inital commit

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+// set configuration for app
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.path}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => { 
+        if (err) {
+            console.log('Unable to log on server.log.txt');
+        }
+    })
+    next();
+});
+
+app.use((req, res) => {
+    res.render('maintenance.hbs');
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/home', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home',
+        welcomeMessage: 'Welcome to my town',
+        author(name) {
+          return name;
+      }
+    })
+  });
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+        author(name) {
+            return name;
+        }
+    });
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        status: 'fail',
+        message: 'Unable to get data'
+    });
+})
+
+app.listen(3000,(error, good) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('App is running at the port 3000');
+    }
+});
